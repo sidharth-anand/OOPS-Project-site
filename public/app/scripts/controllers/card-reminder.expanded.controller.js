b715abdd323b0711596062042ca22c432f5f9db7
yashyenugu <68002151+yashyenugu@users.noreply.github.com>
2020-11-16 19:40:43 +0530

    Add function to remove Reminders

--- public/app/scripts/controllers/card-reminder.expanded.controller.js
+++ public/app/scripts/controllers/card-reminder.expanded.controller.js
@@ -14,10 +14,27 @@
         ctrl.time = "";
 
         ctrl.addReminder = function(){
-            ctrl.reminderList.push({date: ctrl.date, time: ctrl.time});
+            ctrl.reminderList.push({date: ctrl.date, time: ctrl.time, remove:false});
             ctrl.date = "";
             ctrl.time = "";
         }
+
+        ctrl.removeReminder = function(reminder){
+            let oldList = ctrl.reminderList;
+            reminder.remove = true;
+            ctrl.reminderList = [];
+            angular.forEach(oldList,function(x){
+                if (!x.remove){
+                    ctrl.reminderList.push(x)
+                }
+            })
+        };
+
+        ctrl.removeAll = function() {
+            ctrl.reminderList = [];
+        };
+
+        ctrl.showRemoveAll = Boolean(ctrl.reminderList.length)
     }
 
 })();
