ec79bf0c2fa9becebbc256449a23208407005803
Sidharth Anand <sidharthanand85@gmail.com>
2020-11-16 14:41:59 +0530

    Added register page

--- public/app/scripts/controllers/register.controller.js
+++ public/app/scripts/controllers/register.controller.js
@@ -1,6 +1,12 @@
 (function(){
     'use strict';
 
-    
+    let App = angular.module("app");
 
+    App.controller("registerController", registerController);
+    registerController.$inject = [];    
+
+    function registerController() {
+
+    }
 })();
--- public/dist/oops-site.js
+++ public/dist/oops-site.js
@@ -713,8 +713,14 @@
 (function(){
     'use strict';
 
-    
+    let App = angular.module("app");
 
+    App.controller("registerController", registerController);
+    registerController.$inject = [];    
+
+    function registerController() {
+
+    }
 })();;
 (function(){
     'use strict';
