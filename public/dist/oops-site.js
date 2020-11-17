18b35625c523cc4011db96d232c0084d8bc78dc0
Sidharth Anand <sidharthanand85@gmail.com>
2020-11-17 11:10:25 +0530

    Changed login page to dark theme

--- public/dist/oops-site.js
+++ public/dist/oops-site.js
@@ -718,6 +718,16 @@
                         text: "asdvbnq 123 sadkf aerj nmsd fwkreh zd "
                     }
                 ]
+            },
+            {
+                name: "Cards 2",
+                cards: [
+                    {
+                        name: "Text Card",
+                        type: "Text",
+                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                    },
+                ]
             }
         ];
     }
