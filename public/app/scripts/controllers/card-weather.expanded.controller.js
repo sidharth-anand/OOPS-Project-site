7834cdb858c47588af4f51601fce27946ccd605b
Yash <yashyenugu@gmail.com>
2020-11-28 21:22:39 +0530

    Added final touches for weather card

--- public/app/scripts/controllers/card-weather.expanded.controller.js
+++ public/app/scripts/controllers/card-weather.expanded.controller.js
@@ -11,6 +11,7 @@
 
         ctrl.weather = $scope.cardExpandedController.data.weather;
 
+        ctrl.city = "";
         ctrl.requestWeatherByCity = function(town){
             var URL = 'http://api.openweathermap.org/data/2.5/weather?';
       
@@ -27,19 +28,20 @@
             };
             return $http(request);
         }
-        ctrl.weather.status = "";
-        ctrl.requestWeatherByCity('hyderabad').then(function(response){
-            ctrl.weather = response;
-            ctrl.weather.city = response.data.name;
-            ctrl.weather.temp = response.data.main.temp;
-            ctrl.weather.feelsLike = response.data.main.feels_like;
-            ctrl.weather.max = response.data.main.temp_max;
-            ctrl.weather.min = response.data.main.temp_min;
-            ctrl.weather.description = response.data.weather[0].description;
-            ctrl.weather.icon = `https://openweathermap.org/img/wn/${response.data.weather[0]["icon"]}@2x.png`;
-            console.log(response.data)
-            console.log(response.data.main.temp)
+
+        ctrl.getWeather = function(city){
+            ctrl.requestWeatherByCity(city).then(function(response){
+                ctrl.weather = response;
+                ctrl.weather.city = response.data.name;
+                ctrl.weather.temp = "Temperature: "+response.data.main.temp+" °C";
+                ctrl.weather.feelsLike ="Feels Like: "+ response.data.main.feels_like+" °C";
+                ctrl.weather.maxAndMin = "Max: "+response.data.main.temp_max+" °C | Min: "+response.data.main.temp_min+" °C";
+                ctrl.weather.description = response.data.weather[0].description;
+                ctrl.weather.icon = `https://openweathermap.org/img/wn/${response.data.weather[0]["icon"]}@2x.png`;
+                console.log(response.data)
         });
+        ctrl.city = "";
+    }
     }
 
 })();
